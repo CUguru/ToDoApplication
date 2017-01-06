5bda4b97c6d38f82887d69df06045d8ab1206745
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-06 14:20:49 -0500

    new boilerplate code for react applications

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'); // have acces to the whole express API
+
+// Creat our own app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+app.listen(PORT, function() {
+  console.log("express server is up on port " + PORT);
+});

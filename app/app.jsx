6b766b3397b5a1fe1cd52bd2e6cfce123d7fccdd
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-06 15:03:14 -0500

    add the components for the todo app

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+var MainApp = require('MainApp');
 
 
 // app css
@@ -9,10 +9,6 @@ require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      
-    </Route>
-  </Router>,
+  <MainApp/>,
   document.getElementById('app')
 );
